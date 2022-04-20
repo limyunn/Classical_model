9e4ba058add55204918c86d8841f173ea968d207
limyunn <1569233527@qq.com>
2022-04-20 20:32:47 +0800

    model

--- InceptionNet.py
+++ InceptionNet.py
@@ -123,7 +123,33 @@ if __name__ == '__main__':
     y=model(x)
     print(y)
 
-
+# class InceptionAux(layers.Layer):
+#     def __init__(self, num_classes, **kwargs):
+#         super(InceptionAux, self).__init__(**kwargs)
+#         self.averagePool = layers.AvgPool2D(pool_size=5, strides=3)
+#         self.conv = layers.Conv2D(128, kernel_size=1, activation="relu")
+#
+#         self.fc1 = layers.Dense(1024, activation="relu")
+#         self.fc2 = layers.Dense(num_classes)
+#         self.softmax = layers.Softmax()
+#
+#     def call(self, inputs, **kwargs):
+#         # aux1: N x 512 x 14 x 14, aux2: N x 528 x 14 x 14
+#         x = self.averagePool(inputs)
+#         # aux1: N x 512 x 4 x 4, aux2: N x 528 x 4 x 4
+#         x = self.conv(x)
+#         # N x 128 x 4 x 4
+#         x = layers.Flatten()(x)
+#         x = layers.Dropout(rate=0.5)(x)
+#         # N x 2048
+#         x = self.fc1(x)
+#         x = layers.Dropout(rate=0.5)(x)
+#         # N x 1024
+#         x = self.fc2(x)
+#         # N x num_classes
+#         x = self.softmax(x)
+#
+#         return x
 
 
 #---------------------------------------------------------
